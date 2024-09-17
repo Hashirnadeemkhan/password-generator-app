19bf72c6278d816d52b46c71ff5d43090d1fe84e
Hashir Nadeem Khan <khashir657@gmail.com>
2024-09-17 12:24:25 +0500

    this is a password generator app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import GeneratePasswordComponent from "@/components/password-generator";
+
+export default function Home() {
+  return (
+    <div>
+     <GeneratePasswordComponent/>
+    </div>
+  );
+}
